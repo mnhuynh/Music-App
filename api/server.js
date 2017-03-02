8bf2c00e5a106eaa31ee086c56fdc6d0e7222b91
mnhuynh <mazie.huynh@gmail.com>
2017-03-02 11:35:01 -0500

    save

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const http = require('http');
 
 let clientID = '3c6fe50146e44fd285baa70b52cbed2b'
@@ -8,6 +9,12 @@ let redirectURI = 'http://localhost:8080/callback'
 
 let baseURL = 'https://api.spotify.com'
 
+// search bar
+// playlist
+
+app.get('/', (req, res) => {
+    res.json('api page')
+})
 
 app.listen(8080, () => {
     console.log('Server Started on http://localhost:8080');
