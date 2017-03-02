beb6f72774c6207290413b9ac70f681a93e85e12
mnhuynh <mazie.huynh@gmail.com>
2017-03-02 10:25:11 -0500

    save

--- back/server.js
+++ back/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+
+// client ID = 3c6fe50146e44fd285baa70b52cbed2b
+// client secret = 53eed7e2a2aa47de9f31491a9b27fdae
 
 app.listen(8080, () => {
     console.log('Server Started on http://localhost:8080');
