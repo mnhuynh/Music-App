560e17101981dda3744a27c8cd2965bd6ced07a2
mnhuynh <mazie.huynh@gmail.com>
2017-03-03 12:52:14 -0500

    save

--- front/src/components/AlbumsList.js
+++ front/src/components/AlbumsList.js
@@ -5,22 +5,22 @@ class AlbumsList extends Component {
     render() {
         return (
             <div className="container">
-                <h1>Albums</h1>
+                <h1 className="album" >Albums</h1>
                 <div className="row">
                     <div className="col-xs-4 col-sm-4 col-md-4 card-container">
                         <Link to="songs/spice">
-                            <h3>Play Spice</h3>
+                            <h3 className="albumTitle">Play Spice</h3>
                             <img className="img-fluid" src="../img/spice.jpg" alt="" />
                         </Link>
                     </div>
                     <div className="col-xs-4 col-sm-4 col-md-4 card-container">
-                        <h3>Play Spice World</h3>
+                        <h3 className="albumTitle">Play Spice World</h3>
                         <Link to="songs/spiceworld">
                             <img className="img-fluid" src="../img/spiceworld.jpg" alt="" />
                         </Link>
                     </div>
                     <div className="col-xs-4 col-sm-4 col-md-4 card-container">
-                        <h3>Play Forever</h3>
+                        <h3 className="albumTitle">Play Forever</h3>
                         <Link to="songs/forever">
                             <img className="img-fluid" src="../img/forever.png" alt="" />
                         </Link>
--- front/src/components/Spice.js
+++ front/src/components/Spice.js
@@ -33,9 +33,16 @@ class Spice extends Component {
         })
         return (
             <div className="container-row">
+                <img className="innerImg" src="../img/spiceGirlsJump.jpg" alt=""/>
                 <ul className="albumsList">
                     {spiceJSX}
                 </ul>
+                <div className="albumDescription">
+                    <h3 className="words">Spice is the debut studio album by British girl group the Spice Girls. It was first released on 19 September 1996 by Virgin Records. 
+                        The album was recorded at Olympic Studios in Barnes, London between 1995 and 1996, by producers Matt Rowe and Richard Stannard, and the production duo Absolute. 
+                        The album is a pop record with an inclusion of styles such as dance, R&B and hip hop. It is considered to be the record that brought teen pop back, opening the doors for a wave of teen pop artists. 
+                        Conceptually, the album centered on the idea of Girl Power, and during that time was compared to Beatlemania.</h3>
+                </div>
             </div>
 
         )
