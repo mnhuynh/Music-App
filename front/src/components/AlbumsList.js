f802251b430e41e91ef7c06891e893a47162d502
mnhuynh <mazie.huynh@gmail.com>
2017-03-05 00:18:10 -0500

    save

--- front/src/components/AlbumsList.js
+++ front/src/components/AlbumsList.js
@@ -24,19 +24,19 @@ class AlbumsList extends Component {
                     <div className="col-xs-4 col-sm-4 col-md-4 card-container">
                         <Link className="enter" to="songs/spice">
                             <h3 className="albumTitle" title="Click Me!">Spice</h3>
-                            <img className="img-fluid" src="../img/spice.jpg" alt="" />
+                            <img className="img-fluid" src="../img/spice.jpg" title="Click Me!" alt="" />
                         </Link>
                     </div>
                     <div className="col-xs-4 col-sm-4 col-md-4 card-container">
                         <Link className="enter" to="songs/spiceworld">
                             <h3 className="albumTitle" title="Click Me!">Spice World</h3>
-                            <img className="img-fluid" src="../img/spiceworld.jpg" alt="" />
+                            <img className="img-fluid" src="../img/spiceworld.jpg" title="Click Me!" alt="" />
                         </Link>
                     </div>
                     <div className="col-xs-4 col-sm-4 col-md-4 card-container">
                         <Link className="enter" to="songs/forever">
                             <h3 className="albumTitle" title="Click Me!">Forever</h3>
-                            <img className="img-fluid" src="../img/forever.png" alt="" />
+                            <img className="img-fluid" src="../img/forever.png" title="Click Me!" alt="" />
                         </Link>
                     </div>
                 </div>
